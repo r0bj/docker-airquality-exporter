afde44690c3de8272ada5a4c1b7fe494baa0e289
rob <rob@unx.pl>
2020-12-02 16:19:54 -0800

    separate metrics

--- airquality-exporter.py
+++ airquality-exporter.py
@@ -6,7 +6,8 @@ from prometheus_client import start_http_server, Gauge
 import serial
 import time
 
-pm = Gauge('airquality_pm', 'Airquality PM metric', ['type'])
+pm25 = Gauge('airquality_pm25', 'Airquality PM 2.5 metric', ['type'])
+pm10 = Gauge('airquality_pm10', 'Airquality PM 10 metric', ['type'])
 
 ser = serial.Serial('/dev/ttyUSB0')
 
@@ -19,8 +20,8 @@ def process_request():
 	pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
 	pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
-	pm.labels('pm2.5').set(pmtwofive)
-	pm.labels('pm10').set(pmten)
+	pm25.labels('pm2.5').set(pmtwofive)
+	pm10.labels('pm10').set(pmten)
 
 	time.sleep(30)
 
