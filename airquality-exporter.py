f17b961e108d7539ee0c00b57e646530f58bf8b6
rob <rob@unx.pl>
2020-12-02 17:04:59 -0800

    consolidate metrics

--- airquality-exporter.py
+++ airquality-exporter.py
@@ -6,8 +6,7 @@ from prometheus_client import start_http_server, Gauge
 import serial
 import time
 
-pm25 = Gauge('airquality_pm25', 'Airquality PM 2.5 metric', ['type'])
-pm10 = Gauge('airquality_pm10', 'Airquality PM 10 metric', ['type'])
+pm = Gauge('airquality_pm', 'Airquality PM metric', ['type'])
 
 ser = serial.Serial('/dev/ttyUSB0')
 
@@ -20,8 +19,8 @@ def process_request():
 	pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
 	pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
-	pm25.labels('pm2.5').set(pmtwofive)
-	pm10.labels('pm10').set(pmten)
+	pm.labels('pm2.5').set(pmtwofive)
+	pm.labels('pm10').set(pmten)
 
 	time.sleep(30)
 
